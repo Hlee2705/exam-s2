749b0d1640185f522b902405b7fcc93a0b1ef6c2
Hlee2705 <randrianasolonomenavalisoa@gmail.com>
2025-07-14 14:04:47 +0300

    avec upload

--- page/upload_image.php
+++ page/upload_image.php
@@ -1,4 +1,21 @@
 
-<section>
+<section class="uploader">
+    <div class="upload-container">
+    <h2>Ajouter un objet</h2>
+    <form action="../controleur/t_pub.php" method="POST" enctype="multipart/form-data">
+      <div class="form-group">
+        <label for="nom_objet">Nom de l'objet</label>
+        <input type="text" id="nom_objet" name="nom_objet" placeholder="Nom de l'objet" required>
+      </div>
 
+      <div class="form-group">
+        <label for="media">Fichier (image ou vidéo)</label>
+        <input type="file" id="media" name="fichier" accept="image/*,video/*" required>
+      </div>
+      <button type="submit" class="btn-upload">Ajouter</button>
+    </form>
+    <div class="back-link">
+      <a href="list_object.php">⬅ Retour </a>
+    </div>
+  </div>
 </section>
