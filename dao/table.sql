824b61f2d450535c4ff4bd251f016c4df175bbfd
Hlee2705 <randrianasolonomenavalisoa@gmail.com>
2025-07-14 10:03:32 +0300

    insetion des donnes de la vesion 1

--- dao/table.sql
+++ dao/table.sql
@@ -51,4 +51,66 @@ VALUES ("esthétique"),
 ("mécanique"),
 ("cuisine");
 
+-- Objets pour Mahatoky (id_menbre = 1)
+INSERT INTO objet(nom_objet, id_categorie, id_membre) VALUES
+("Rouge à lèvres", 1, 1),
+("Tournevis", 2, 1),
+("Bougie d’allumage", 3, 1),
+("Mixeur", 4, 1),
+("Fond de teint", 1, 1),
+("Perceuse", 2, 1),
+("Clé à molette", 3, 1),
+("Poêle", 4, 1),
+("Crayon à sourcils", 1, 1),
+("Scie sauteuse", 2, 1);
 
+-- Objets pour Valisoa (id_menbre = 2)
+INSERT INTO objet(nom_objet, id_categorie, id_membre) VALUES
+("Filtre à huile", 3, 2),
+("Casserole", 4, 2),
+("Mascara", 1, 2),
+("Tournevis cruciforme", 2, 2),
+("Disque de frein", 3, 2),
+("Four", 4, 2),
+("Poudre libre", 1, 2),
+("Marteau", 2, 2),
+("Amortisseur", 3, 2),
+("Mixeur plongeant", 4, 2);
+
+-- Objets pour Yoann (id_menbre = 3)
+INSERT INTO objet(nom_objet, id_categorie, id_membre) VALUES
+("Crème hydratante", 1, 3),
+("Tournevis plat", 2, 3),
+("Alternateur", 3, 3),
+("Batteur électrique", 4, 3),
+("Vernis à ongles", 1, 3),
+("Clé Allen", 2, 3),
+("Pompe à eau", 3, 3),
+("Grille-pain", 4, 3),
+("Lotion tonique", 1, 3),
+("Scie circulaire", 2, 3);
+
+-- Objets pour Nekena (id_menbre = 4)
+INSERT INTO objet(nom_objet, id_categorie, id_membre) VALUES
+("Radiateur moteur", 3, 4),
+("Cocotte-minute", 4, 4),
+("Baume à lèvres", 1, 4),
+("Pince multiprise", 2, 4),
+("Boîte de vitesses", 3, 4),
+("Micro-ondes", 4, 4),
+("Crème solaire", 1, 4),
+("Tournevis électrique", 2, 4),
+("Courroie de distribution", 3, 4),
+("Robot pâtissier", 4, 4);
+
+INSERT INTO emprunt(id_objet, id_membre, date_emprunt, date_retour) VALUES
+(1, 2, '2025-07-01 10:00:00', '2025-07-05 10:00:00'),
+(5, 3, '2025-07-03 14:00:00', '2025-07-07 14:00:00'),
+(12, 1, '2025-07-04 09:00:00', '2025-07-10 09:00:00'),
+(18, 4, '2025-07-02 16:30:00', '2025-07-06 16:30:00'),
+(25, 1, '2025-07-05 08:15:00', '2025-07-11 08:15:00'),
+(30, 2, '2025-07-06 17:00:00', '2025-07-13 17:00:00'),
+(33, 3, '2025-07-07 13:00:00', '2025-07-14 13:00:00'),
+(37, 4, '2025-07-08 11:45:00', '2025-07-12 11:45:00'),
+(10, 3, '2025-07-01 09:00:00', '2025-07-04 09:00:00'),
+(22, 2, '2025-07-02 15:00:00', '2025-07-09 15:00:00');
